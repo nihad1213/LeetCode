685d556fe3c153b73f42fd0cb02ae0c11e90fb64
Nihad <99197650+nihad1213@users.noreply.github.com>
2025-12-01 02:41:07 +0400

    cpp solution created

---
+++ 09.PalindromeNumber/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversed || x == reversed / 10;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.isPalindrome(121) << endl;
+    cout << s.isPalindrome(-121) << endl;
+    cout << s.isPalindrome(10) << endl;
+    cout << s.isPalindrome(123454321) << endl;
+    return 0;
+}
