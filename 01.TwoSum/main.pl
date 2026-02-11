d59e83eb25667834734eb39db16c48a4b085e4fe
Nihad <99197650+nihad1213@users.noreply.github.com>
2026-02-11 15:06:02 +0400

    Perl Solution added
    Implement twoSum function to find indices of two numbers that add up to the target. Added test cases to validate functionality.

--- 01.TwoSum/main.pl
+++ 01.TwoSum/main.pl
@@ -1,1 +1,17 @@
-# TEST
+use Data::Dumper;
+
+sub twoSum {
+    my ($nums, $target) = @_;
+    
+    for (my $i = 0; $i < scalar(@$nums); $i++) {
+        for (my $j = $i + 1; $j < scalar(@$nums); $j++) {
+            if ($nums->[$i] + $nums->[$j] == $target) {
+                return [$i, $j];
+            }
+        }
+    }
+}
+
+print Dumper(twoSum([2, 7, 11, 15], 9));
+print Dumper(twoSum([3, 2, 4], 6));
+print Dumper(twoSum([3, 3], 6));
